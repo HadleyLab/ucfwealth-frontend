6fcf4744b09e9cc9dc87596607d8d2557569b886
Alexander Pavlushkin <pavlushkin87@gmail.com>
2021-01-25 13:09:47 +0700

    Stash routes all

--- web/src/containers/PatientApp/QuestionnaireForm/QuestionnaireForm.tsx
+++ web/src/containers/PatientApp/QuestionnaireForm/QuestionnaireForm.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
 
-import {isSuccess} from 'aidbox-react/src/libs/remoteData';
-
-
 export const QuestionnaireForm = () => {
-    
-    const questionnireRD = useService();
-    isSuccess();
-
-    return <div>Lol</div>
-}
+    return <div>Quest form</div>;
+};
