a6709d06d536597a2c6c4693ad648bb2d018c1af
atuonufure <atuonufure@gmail.com>
2022-06-07 08:22:46 +0200

    Update header at patient storyline

--- web/src/components/PatientDataInfo/index.tsx
+++ web/src/components/PatientDataInfo/index.tsx
@@ -13,27 +13,42 @@ interface Props {
 
 export const PatientDataInfo = ({ patient }: Props) => {
     const renderContent = (column = 2) => (
-        <Descriptions size="middle" column={column}>
-            <Descriptions.Item label="Address">
-                {patient.address?.[0].country} {patient.address?.[0].state}{' '}
-                {patient.address?.[0].city} {patient.address?.[0].line?.[0]}
+        <Descriptions size="middle" column={column} colon={false}>
+            <Descriptions.Item label="Address" labelStyle={leftColumnLabelStyle}>
+                {patient.address
+                    ? `${patient.address[0].country} {patient.address[0].state} 
+                ${patient.address[0].city} ${patient.address[0].line?.[0]}`
+                    : 'Empty'}
             </Descriptions.Item>
-            <Descriptions.Item label="Medical Record Number">
-                {patient.identifier?.[1].value}
+            <Descriptions.Item label="Medical Record Number" labelStyle={rightColumnLabelStyle}>
+                {patient.identifier?.[1].value || 'Empty'}
             </Descriptions.Item>
-            <Descriptions.Item label="Birth date">{patient.birthDate}</Descriptions.Item>
-            <Descriptions.Item>{patient.identifier?.[1].system}</Descriptions.Item>
-            <Descriptions.Item label="Birth place">
-                {patient.extension?.[4]['valueAddress'].country}{' '}
-                {patient.extension?.[4]['valueAddress'].state}{' '}
-                {patient.extension?.[4]['valueAddress'].city}
+            <Descriptions.Item label="Birth date" labelStyle={leftColumnLabelStyle}>
+                {patient.birthDate || 'Empty'}
             </Descriptions.Item>
-            <Descriptions.Item label="Created at">
-                {patient.meta?.['createdAt'].slice(0, 10)}
+            <Descriptions.Item label="External Patient Resource" labelStyle={rightColumnLabelStyle}>
+                {patient.identifier?.[1].system ? (
+                    <a href={patient.identifier?.[1].system} className={s.externalPatientResource}>
+                        {patient.identifier?.[1].system}
+                    </a>
+                ) : (
+                    'Empty'
+                )}
             </Descriptions.Item>
-            <Descriptions.Item label="Ethnicity">
-                {patient.extension?.[0].extension?.[1].valueString} /{' '}
-                {patient.extension?.[1].extension?.[1].valueString}
+            <Descriptions.Item label="Birth place" labelStyle={leftColumnLabelStyle}>
+                {patient.extension
+                    ? `${patient.extension[4]['valueAddress'].country} 
+                ${patient.extension[4]['valueAddress'].state} 
+                ${patient.extension[4]['valueAddress'].city}`
+                    : 'Empty'}
+            </Descriptions.Item>
+            <Descriptions.Item label="Created at" labelStyle={rightColumnLabelStyle}>
+                {patient.meta?.['createdAt'].slice(0, 10) || 'Empty'}
+            </Descriptions.Item>
+            <Descriptions.Item label="Ethnicity" labelStyle={leftColumnLabelStyle}>
+                {patient.extension
+                    ? `${patient.extension[0].extension?.[1].valueString} / ${patient.extension[1].extension?.[1].valueString}`
+                    : 'Empty'}
             </Descriptions.Item>
         </Descriptions>
     );
@@ -42,12 +57,18 @@ export const PatientDataInfo = ({ patient }: Props) => {
         <PageHeader
             className={s.pageHeader}
             onBack={() => window.history.back()}
-            title={`${patient.name?.[0].prefix} ${patient.name?.[0].given} 
-            ${patient.name?.[0].family}`}
-            subTitle={`SSN: ${patient.identifier?.[2].value}`}
+            title={
+                patient.name
+                    ? `${patient.name[0].prefix} ${patient.name[0].given} ${patient.name[0].family}`
+                    : 'Name not provided'
+            }
+            subTitle={patient.identifier ? `SSN: ${patient.identifier[2].value}` : 'SSN: Empty'}
         >
             <Content>{renderContent()}</Content>
         </PageHeader>
     );
 };
 
+const leftColumnLabelStyle = { width: 98, fontWeight: 700 };
+
+const rightColumnLabelStyle = { width: 208, fontWeight: 700 };
