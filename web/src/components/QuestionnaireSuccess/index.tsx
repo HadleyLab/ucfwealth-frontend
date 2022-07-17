75d8a1214863a11f6f428a693a6d820123f034f5
atuonufure <atuonufure@gmail.com>
2022-07-17 14:20:37 +0200

    Update questionnaire last step

--- web/src/components/QuestionnaireSuccess/index.tsx
+++ web/src/components/QuestionnaireSuccess/index.tsx
@@ -1,3 +1,5 @@
+import { Button } from 'antd';
+
 import { SuccessIcon } from 'src/images/SuccessIcon';
 import { sharedPatientId } from 'src/sharedState';
 
@@ -9,10 +11,30 @@ export const QuestionnaireSuccess = () => {
         <div className={s.wrapper}>
             <SuccessIcon />
             <div className={s.thankYou}>
-                Thank you!
-                <br />
-                Please upload your DICOM files{' '}
-                <a href={`https://uci.beda.software/${patientId.id}`}>here</a>
+                <div className={s.whatsNext}>What's next?</div>
+                <div className={s.step}>
+                    1. Do you have a CD with the DICOM images?
+                    <br />
+                    If so, please upload it into the system.
+                </div>
+                <div className={s.step}>
+                    2. After the files are uploaded, you'll get your crypto wallet access.
+                    <br />
+                    Please, save it securely for future usage.
+                </div>
+                <div className={s.step}>
+                    3. The study coordinator will review the files within a few days.
+                    <br />
+                    If the images fit into the study, you'll will receive an email with the access
+                    to your NFT.
+                </div>
+                <Button
+                    className={s.uploadImages}
+                    type="primary"
+                    href={`https://uci.beda.software/${patientId.id}`}
+                >
+                    <span className={s.uploadImagesSpan}>Upload images</span>
+                </Button>
             </div>
         </div>
     );
