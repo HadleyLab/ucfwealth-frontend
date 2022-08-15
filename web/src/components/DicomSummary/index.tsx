f0d4f25c844d456e5c61331dc9cc2abe4ef79a5b
atuonufure <atuonufure@gmail.com>
2022-08-15 08:33:05 +0200

    Add previews for dicom files

--- web/src/components/DicomSummary/index.tsx
+++ web/src/components/DicomSummary/index.tsx
@@ -1,3 +1,8 @@
+import { useService } from 'aidbox-react/src/hooks/service';
+import { isFailure } from 'aidbox-react/src/libs/remoteData';
+import { service } from 'aidbox-react/src/services/service';
+
+import { RenderRemoteData } from 'src/components/RenderRemoteData';
 import { downloadFile, removePatientIdFromFileKey } from 'src/utils/patientFileList';
 
 interface Props {
@@ -11,8 +16,13 @@ export const DicomSummary = ({ data }: Props) => {
         <div>
             {data.dicomFileList.length > 0 ? (
                 data.dicomFileList.map((fileKey: string, key: string) => (
-                    <div className={s.title} key={key} onClick={() => downloadFile(fileKey)}>
-                        {removePatientIdFromFileKey(fileKey)}
+                    <div>
+                        <div className={s.title} key={key} onClick={() => downloadFile(fileKey)}>
+                            {removePatientIdFromFileKey(fileKey)}
+                        </div>
+                        <div className={s.showImage}>
+                            <ShowImage fileKey={fileKey} />
+                        </div>
                     </div>
                 ))
             ) : (
@@ -20,4 +30,37 @@ export const DicomSummary = ({ data }: Props) => {
             )}
         </div>
     );
+};
+
+interface ShowImageProps {
+    fileKey: string;
+}
+
+const ShowImage = ({ fileKey }: ShowImageProps) => {
+    const { imageRD } = useShowImage({ fileKey });
+
+    return (
+        <RenderRemoteData remoteData={imageRD}>
+            {(data) => <img src={`data:image/png;base64,${data}`} className={s.image} />}
+        </RenderRemoteData>
+    );
+};
+
+const useShowImage = ({ fileKey }: { fileKey: string }) => {
+    const [imageRD] = useService(async () => {
+        const response = await service({
+            method: 'GET',
+            url: '$get-png-image-base64',
+            params: { key: fileKey },
+        });
+
+        if (isFailure(response)) {
+            console.log(response.status, response.error);
+            return response;
+        }
+
+        return response;
+    }, []);
+
+    return { imageRD };
 };
