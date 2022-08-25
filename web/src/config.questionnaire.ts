3a33e14f0eea1d6d6eac2b978856b26933eb8dc9
atuonufure <atuonufure@gmail.com>
2022-08-25 07:28:08 +0200

    Remove unused config

--- web/src/config.questionnaire.ts
+++
@@ -1,4 +0,0 @@
-export const questionnaireNameList = ['personal-information', 'screening-questions'];
-// 'screening-questions' 'patient-report-baseline'
-
-export const questionnaireTitle = 'COVID-19 Questionnaire';
