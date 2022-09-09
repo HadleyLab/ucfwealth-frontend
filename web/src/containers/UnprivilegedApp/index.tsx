42d9711cbadee6477614c1f43c6aea8f118fa1ab
atuonufure <atuonufure@gmail.com>
2022-09-09 08:20:46 +0200

    Add Redirect from UnprivilegedApp if patient created

--- web/src/containers/UnprivilegedApp/index.tsx
+++ web/src/containers/UnprivilegedApp/index.tsx
@@ -1,8 +1,39 @@
+import { Space, Spin } from 'antd';
+import { useState } from 'react';
+
+import { isFailure, isSuccess } from 'aidbox-react/src/libs/remoteData';
+
+import { useInterval } from 'src/containers/SuperAdminApp/PatientProgressList/Celebrate/useInterval';
+import { getUserInfo } from 'src/services/auth';
+
 export function UnprivilegedApp() {
+    const [checkPatient, setCheckPatient] = useState(true);
+    const checkPatientCreated = async () => {
+        const response = await getUserInfo();
+        if (isFailure(response)) {
+            console.error(response.error);
+            return;
+        }
+        if (isSuccess(response)) {
+            const data = response.data.data;
+            if ('patient' in data) {
+                return true;
+            }
+        }
+        return false;
+    };
+    useInterval(async () => {
+        if (checkPatient) {
+            const patientCreated = await checkPatientCreated();
+            if (patientCreated) {
+                setCheckPatient(false);
+                window.location.reload();
+            }
+        }
+    }, 3000);
     return (
-        <>
-            <h2>UnprivilegedApp</h2>
-            <p>UnprivilegedApp</p>
-        </>
+        <Space size="middle">
+            <Spin size="large" />
+        </Space>
     );
 }
