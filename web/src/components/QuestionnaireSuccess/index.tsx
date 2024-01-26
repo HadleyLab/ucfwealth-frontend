720eb55f0f46d26893b69d2de9bf9785c16a5168
atuonufure <atuonufure@gmail.com>
2024-01-26 08:41:38 +0100

    Remove text about NFT and crypto wallet Refs: https://github.com/HadleyLab/ucfwealth/issues/1

--- web/src/components/QuestionnaireSuccess/index.tsx
+++ web/src/components/QuestionnaireSuccess/index.tsx
@@ -1,4 +1,3 @@
-
 import { FileUploader } from 'src/containers/FileUploader/App';
 import { SuccessIcon } from 'src/images/SuccessIcon';
 
@@ -11,20 +10,9 @@ export const QuestionnaireSuccess = () => {
             <div className={s.thankYou}>
                 <div className={s.whatsNext}>What's next?</div>
                 <div className={s.step}>
-                    1. Do you have a CD with the DICOM images?
+                    Do you have a CD with the DICOM images?
                     <br />
                     If so, please upload it into the system.
-                </div>
-                <div className={s.step}>
-                    2. After the files are uploaded, you'll get your crypto wallet access.
-                    <br />
-                    Please, save it securely for future usage.
-                </div>
-                <div className={s.step}>
-                    3. The study coordinator will review the files within a few days.
-                    <br />
-                    If the images fit into the study, you'll will receive an email with the access
-                    to your NFT.
                 </div>
                 <FileUploader />
             </div>
