d17dba348eae3db190cc91a2d87834a798815820
Elena Grednikova <vesnushkalena@gmail.com>
2024-10-10 19:07:25 +0200

    Update practitioner sidebar menu

--- src/components/BaseLayout/Sidebar/SidebarTop/index.tsx
+++ src/components/BaseLayout/Sidebar/SidebarTop/index.tsx
@@ -4,13 +4,9 @@ import classNames from 'classnames';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 import { EncountersIcon } from '@beda.software/emr/dist/icons/menu/EncountersIcon';
-import { InvoicesIcon } from '@beda.software/emr/dist/icons/menu/InvoicesIcon';
-import { MedicationsIcon } from '@beda.software/emr/dist/icons/menu/MedicationsIcon';
 import { PatientsIcon } from '@beda.software/emr/dist/icons/menu/PatientsIcon';
 import { PractitionersIcon } from '@beda.software/emr/dist/icons/menu/PractitionersIcon';
-import { PrescriptionsIcon } from '@beda.software/emr/dist/icons/menu/PrescriptionsIcon';
 import { QuestionnairesIcon } from '@beda.software/emr/dist/icons/menu/QuestionnairesIcon';
-import { ServicesIcon } from '@beda.software/emr/dist/icons/menu/ServicesIcon';
 import { getToken } from '@beda.software/emr/services';
 import { matchCurrentUserRole, Role } from '@beda.software/emr/utils';
 
@@ -42,17 +38,22 @@ export function SidebarTop(props: Props) {
     const menuItems: RouteItem[] = !isAnonymousUser
         ? matchCurrentUserRole({
               [Role.Admin]: () => [
-                  { label: t`Invoices`, path: '/invoices', icon: <InvoicesIcon /> },
-                  { label: t`Services`, path: '/healthcare-services', icon: <ServicesIcon /> },
-                  { label: t`Encounters`, path: '/encounters', icon: <EncountersIcon /> },
                   { label: t`Patients`, path: '/patients', icon: <PatientsIcon /> },
                   { label: t`Practitioners`, path: '/practitioners', icon: <PractitionersIcon /> },
                   { label: t`Questionnaires`, path: '/questionnaires', icon: <QuestionnairesIcon /> },
+                  {
+                      label: t`Community`,
+                      path: `https://community.ucfwealth.app/auth/oauth2_basic`,
+                      icon: <PatientsIcon />,
+                  },
               ],
               [Role.Practitioner]: () => [
-                  { label: t`Encounters`, path: '/encounters', icon: <EncountersIcon /> },
                   { label: t`Patients`, path: '/patients', icon: <PatientsIcon /> },
-                  { label: t`Questionnaires`, path: '/questionnaires', icon: <QuestionnairesIcon /> },
+                  {
+                      label: t`Community`,
+                      path: `https://community.ucfwealth.app/auth/oauth2_basic`,
+                      icon: <PatientsIcon />,
+                  },
               ],
               [Role.Patient]: (patient) => [
                   { label: t`My Dashboard`, path: `/patients/${patient.id}`, icon: <EncountersIcon /> },
@@ -63,10 +64,12 @@ export function SidebarTop(props: Props) {
                   },
               ],
               [Role.Receptionist]: () => [
-                  { label: t`Scheduling`, path: '/scheduling', icon: <EncountersIcon /> },
-                  { label: t`Invoices`, path: '/invoices', icon: <InvoicesIcon /> },
-                  { label: t`Medications`, path: '/medications', icon: <MedicationsIcon /> },
-                  { label: t`Prescriptions`, path: '/prescriptions', icon: <PrescriptionsIcon /> },
+                  { label: t`Patients`, path: '/patients', icon: <PatientsIcon /> },
+                  {
+                      label: t`Community`,
+                      path: `https://community.ucfwealth.app/auth/oauth2_basic`,
+                      icon: <PatientsIcon />,
+                  },
               ],
           })
         : [];
