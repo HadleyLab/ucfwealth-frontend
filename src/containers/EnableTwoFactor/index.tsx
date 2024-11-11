52fb0574201b2422ee8756602ec296a541fdbc15
Elena Grednikova <vesnushkalena@gmail.com>
2024-11-11 23:29:10 +0100

    Remove enable 2fa extra step

--- src/containers/EnableTwoFactor/index.tsx
+++ src/containers/EnableTwoFactor/index.tsx
@@ -1,10 +1,9 @@
 import { Alert, Button, Input } from 'antd';
-import React from 'react';
-
-import { isSuccess } from 'aidbox-react/lib/libs/remoteData';
-import { formatError } from 'aidbox-react/lib/utils/error';
+import React, { useEffect } from 'react';
 
 import { Title } from '@beda.software/emr/components';
+import { formatError } from '@beda.software/fhir-react';
+import { RemoteData, isNotAsked, isSuccess, loading, notAsked } from '@beda.software/remote-data';
 
 import { requestTwoFactor, confirmTwoFactor } from 'src/services/two-factor';
 
@@ -22,17 +21,19 @@ function useTwoFactor(props: Props) {
     const [token, setToken] = React.useState('');
     const [error, setError] = React.useState<string | undefined>();
     const { reload } = props;
-    const [confirmationState, setConfirmationState] = React.useState<ConfirmationState | null>(null);
+    const [confirmationState, setConfirmationState] = React.useState<RemoteData<ConfirmationState | null>>(notAsked);
 
     const request = async (transport?: string) => {
+        setConfirmationState(loading);
         const response = await requestTwoFactor({ transport });
 
         if (isSuccess(response)) {
-            setConfirmationState(response.data);
+            setConfirmationState(response);
         } else {
             alert(formatError(response.error));
         }
     };
+
     const confirm = async () => {
         setError(undefined);
 
@@ -45,50 +46,48 @@ function useTwoFactor(props: Props) {
         }
     };
 
+    useEffect(() => {
+        (async () => {
+            if (isNotAsked(confirmationState)) {
+                await request('email');
+            }
+        })();
+    }, [confirmationState]);
+
     return { confirmationState, request, confirm, token, setToken, error };
 }
 
 export function EnableTwoFactor(props: Props) {
-    const { confirmationState, request, confirm, setToken, token, error } = useTwoFactor(props);
-
-    if (confirmationState) {
-        return (
-            <S.Container>
-                <form
-                    onSubmit={(event) => {
-                        event.preventDefault();
-                        return confirm();
-                    }}
-                >
-                    <S.Form>
-                        <Title level={5}>Input the token to finish setting up two-factor authentication</Title>
-                        {error && <Alert message={error} type="error" />}
-                        <Input
-                            type="number"
-                            autoComplete="off"
-                            autoFocus
-                            name="token"
-                            placeholder="Input token here"
-                            value={token}
-                            onChange={(event) => setToken(event.currentTarget.value)}
-                        />
-                        <Button type="primary" size="large" htmlType="submit">
-                            Confirm
-                        </Button>
-                    </S.Form>
-                </form>
-            </S.Container>
-        );
-    }
+    const { confirm, setToken, token, error } = useTwoFactor(props);
 
     return (
         <S.Container>
-            <S.Form>
-                <Title level={5}>Do you want to enable two-factor authentication using email?</Title>
-                <Button type="primary" size="large" onClick={() => request('email')}>
-                    Enable
-                </Button>
-            </S.Form>
+            <form
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    return confirm();
+                }}
+            >
+                <S.Form>
+                    <Title level={5}>
+                        Check your email for the token to finish setting up two-factor authentication. Input the token
+                        you received
+                    </Title>
+                    {error && <Alert message={error} type="error" />}
+                    <Input
+                        type="number"
+                        autoComplete="off"
+                        autoFocus
+                        name="token"
+                        placeholder="Input token here"
+                        value={token}
+                        onChange={(event) => setToken(event.currentTarget.value)}
+                    />
+                    <Button type="primary" size="large" htmlType="submit">
+                        Confirm
+                    </Button>
+                </S.Form>
+            </form>
         </S.Container>
     );
 }
