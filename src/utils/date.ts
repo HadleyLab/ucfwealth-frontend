7c7481865b004a08adf5986bf38bd0e392e26e0c
Elena Grednikova <vesnushkalena@gmail.com>
2024-11-19 20:09:29 +0100

    Add date utils

---
+++ src/utils/date.ts
@@ -0,0 +1,11 @@
+export const humanDate = 'MM/DD/YYYY';
+export const humanDateYearMonth = 'MMMM YYYY';
+export const humanTime = 'HH:mm';
+export const humanDateTime = 'MM/DD/YYYY HH:mm';
+
+export const dateFormats = {
+    humanDate,
+    humanDateYearMonth,
+    humanTime,
+    humanDateTime,
+};
