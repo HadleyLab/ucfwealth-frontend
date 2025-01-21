b13fcb2e018e74d81aeb8a9b0d2add301f4ad80d
Elena Tretiakova <vesnushkalena@gmail.com>
2025-01-21 10:55:12 +0100

    Update dashboard custom card background

--- src/containers/PatientDetails/Dashboard/styles.ts
+++ src/containers/PatientDetails/Dashboard/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const S = {
     Content: styled.div`
         border-radius: 10px;
-        background-color: ${({ theme }) => theme.neutralPalette.gray_2};
+        background-color: ${({ theme }) => theme.neutralPalette.gray_1};
         color: ${({ theme }) => theme.neutralPalette.gray_13};
         border: 1px solid ${({ theme }) => theme.antdTheme?.colorBorderSecondary};
         padding: 10px 16px;
